31f3599fa1814fce7ec695d587fdb65c05c78bdf
TAIDARXXX <aidar99921@gmail.com>
2022-05-28 19:51:39 +0500

    Добавьте файлы проекта.

---
+++ 14/camera.h
@@ -0,0 +1,39 @@
+#ifndef CAMERA_H
+#define	CAMERA_H
+
+#include "math_3d.h"
+
+/*Это объявление класса камеры. Он хранит 3 свойства, которые характеризуют камеру - позиция, направление и верхний вектор.*/
+class Camera
+{
+public:
+
+    Camera();
+
+    Camera(const Vector3f& Pos, const Vector3f& Target, const Vector3f& Up);
+
+    bool OnKeyboard(int Key);
+
+    const Vector3f& GetPos() const
+    {
+        return m_pos;
+    }
+
+    const Vector3f& GetTarget() const
+    {
+        return m_target;
+    }
+
+    const Vector3f& GetUp() const
+    {
+        return m_up;
+    }
+
+private:
+
+    Vector3f m_pos;
+    Vector3f m_target;
+    Vector3f m_up;
+};
+
+#endif	/* CAMERA_H */
